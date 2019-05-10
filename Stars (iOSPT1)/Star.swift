1aa400d012f688ea2745e7e62072e629e0048742
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-09 20:20:35 -0700

    implemented filemanager to save data and load

---
+++ Stars (iOSPT1)/Star.swift
@@ -0,0 +1,14 @@
+//
+//  Star.swift
+//  Stars (iOSPT1)
+//
+//  Created by Dongwoo Pae on 5/9/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+
+struct Star: Codable {   //this works for JSON as well
+    var name: String
+    var distance: Double
+}
--- Stars (iOSPT1)/ViewController.swift
+++ Stars (iOSPT1)/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBoutlets and properties
+    
+    @IBOutlet weak var starNameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    @IBOutlet weak var starsTextView: UITextView!
+    
+    let startController = StarController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.listStars()
     }
 
-
+    //IBaction and methods
+    @IBAction func addButtonTapped(_ sender: Any) {
+        guard let name = starNameTextField.text,
+            let distanceString = distanceTextField.text,
+            let distance = Double(distanceString) else {return}
+        
+        self.startController.addStar(named: name, withDistance: distance)
+        self.listStars()
+        self.clearInput()
+    }
+    
+    
+    private func listStars() {
+        var output = ""
+        for star in startController.stars {
+            output += "\(star.name) is \(star.distance) light years away. \n"
+        }
+        starsTextView.text = output
+    }
+    
+    private func clearInput() {
+        self.starNameTextField.text = ""
+        self.distanceTextField.text = ""
+    }
 }
 
